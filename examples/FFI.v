2483817506a7907556fd41a4465a9d1d1c2942ca
nicolas tabareau <nicolas.tabareau@inria.fr>
2019-07-24 16:06:15 +0200

    more in FFI

--- examples/FFI.v
+++ examples/FFI.v
@@ -83,12 +83,10 @@ Definition isHProp_leq_Z : forall (n m p : Z) (e e': n <= m < p), e = e'.
 (* to be done *)
 Admitted. 
 
-Definition to_Z_inj x y : to_Z_modulo x = to_Z_modulo y -> x = y.
+Definition eqZ_ZwB (x y:ZwB) : x.1 = y.1 -> x = y.
 Proof.
-  intro e. assert (to_Z_modulo x = to_Z_modulo y).
-  unfold to_Z_modulo. apply path_sigma_uncurried. unshelve eexists.
-  apply isHProp_leq_Z. etransitivity. apply inverse. apply to_Z_section.
-  rewrite X. apply to_Z_section.
+  intro e. apply path_sigma_uncurried. unshelve eexists.
+  apply isHProp_leq_Z. 
 Defined. 
   
 Definition IsEquiv_to_Z_ : IsEquiv to_Z_modulo := isequiv_adjointify _ of_Z to_Z_section to_Z_retraction.
@@ -120,7 +118,7 @@ Local Open Scope ZwB_scope.
 Definition ZwB_comm (n m: ZwB) : n + m = m + n.
 Proof.
   unfold ZwB_add, ZwB_add_obligation_1. simpl.
-  rewrite (Z.add_comm n.1 m.1). reflexivity. 
+  rewrite Z.add_comm; reflexivity. 
 Defined.
 
 Local Open Scope int63_scope.
@@ -129,9 +127,10 @@ Local Open Scope int63_scope.
 
 Axiom compat_add : add ≈ ZwB_add.
 Definition compat_add' : ZwB_add ≈ add := compat_inverse2 compat_add. 
-  
+
 Hint Extern 0 (add _ _ = _) => eapply compat_add : typeclass_instances.
 Hint Extern 0 (ZwB_add _ _ = _) => eapply compat_add' : typeclass_instances.
+Hint Extern 0 (_ = to_Z_modulo (add _ _)) => eapply compat_add' : typeclass_instances.
 
 (* now property on 2wB_add can be lifted to add on int *)
 
@@ -173,6 +172,30 @@ Definition compat_sub' : ZwB_sub ≈ sub := compat_inverse2 compat_sub.
 Hint Extern 0 (sub _ _ = _) => eapply compat_sub : typeclass_instances.
 Hint Extern 0 (ZwB_sub _ _ = _) => eapply compat_sub' : typeclass_instances.
 
+(* lsl on Z modulo 2^63 *)
+
+Program Definition ZwB_lsl : ZwB -> ZwB -> ZwB :=
+  fun n m => ((n.1 * Z.pow 2 m.1) mod wB ; _).
+Next Obligation.
+  now apply Z.mod_pos_bound. 
+Defined. 
+
+Notation "n << m" := (ZwB_lsl n m) : ZwB_scope.
+
+Arguments ZwB_lsl : simpl never.
+
+(* Axiomatization of compatibility with mul *)
+
+Axiom compat_lsl : lsl ≈ ZwB_lsl.
+Definition compat_lsl' : ZwB_lsl ≈ lsl := compat_inverse2 compat_lsl. 
+  
+Hint Extern 0 (lsl _ _ = _) => eapply compat_lsl : typeclass_instances.
+Hint Extern 0 (ZwB_lsl _ _ = _) => eapply compat_lsl' : typeclass_instances.
+Hint Extern 0 (_ = to_Z_modulo (lsl _ _)) => eapply compat_lsl : typeclass_instances.
+Hint Extern 0 (ZwB_lsl _ _ = _) => eapply compat_lsl' : typeclass_instances.
+
+
+
 (* Test with polynomials *)
 
 Local Open Scope Z_scope.
@@ -193,10 +216,28 @@ Definition compat_eq_int : @eq int63 ≈ @eq ZwB := ltac:(tc).
 Definition compat_eq_ZwB : @eq ZwB ≈ @eq int63 := ltac:(tc). 
 
 Hint Extern 0 (eq ZwB _ _ ≃ _) => eapply compat_eq_ZwB : typeclass_instances.
+Hint Extern 0 (eq int _ _ ≃ _) => eapply compat_eq_int : typeclass_instances.
 Hint Extern 0 (eq int63 _ _ ≃ _) => eapply compat_eq_int : typeclass_instances.
 Hint Extern 0 (eq ZwB _ _ ⋈ _) => eapply compat_eq_ZwB : typeclass_instances.
+Hint Extern 0 (eq int _ _ ⋈ _) => eapply compat_eq_int : typeclass_instances.
 Hint Extern 0 (eq int63 _ _ ⋈ _) => eapply compat_eq_int : typeclass_instances.
 
 Goal eq ZwB 6250600 (poly 50).
   unfold poly. replace_goal. compute. reflexivity. 
 Defined.
+
+Local Open Scope int63_scope.
+
+Ltac change_eq_to_eq := let e := fresh "e" in
+                       match goal with | |- ?X = ?Y => assert (e : Logic.eq X Y) ; [idtac | now destruct e] end.
+
+Definition ZwB_lsr_add_distr x y n :
+  ZwB_lsl (ZwB_add x y) n = ZwB_add (ZwB_lsl x n) (ZwB_lsl y n).
+apply eqZ_ZwB. unfold ZwB_lsl. simpl. 
+rewrite -> Zmult_mod_idemp_l. rewrite <-Zplus_mod.
+eapply ap2; try reflexivity. change_eq_to_eq. 
+auto with zarith. 
+Defined. 
+  
+Definition lsr_add_distr : forall x y n, eq int63 ((x + y) << n) ((x << n) + (y << n)) :=
+  ↑ ZwB_lsr_add_distr.
