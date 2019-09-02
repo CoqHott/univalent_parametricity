6ae2994b6a087afc0345f16209d6f04c597a647b
nicolas tabareau <nicolas.tabareau@inria.fr>
2019-09-02 22:08:49 +0200

    remove almost all admit in DecidableEq

--- theories/DecidableEq.v
+++ theories/DecidableEq.v
@@ -8,7 +8,6 @@ Require Import HoTT UR FP Coq.Program.Tactics.
 
 Class DecidableEq A := { dec_paths : forall a b : A, (a = b) + (a = b -> False)}.
 
-
 (**
 Hedberg theorem is a standard theorem of HoTT: it states that if a
 type [A] has decidable equality, then it is a hSet, i.e. its equality
@@ -141,13 +140,109 @@ Definition path_sum {A B : Type} (z z' : A + B)
   all:elim pq.
 Defined.
 
+Definition dec_hprop A (decA decA' : DecidableEq A) : decA = decA'.
+  apply path_DecidableEq. apply funext. intro a. apply funext. intro a'.
+  apply path_sum. destruct (dec_paths a a'); destruct (dec_paths a a'); auto. 
+  apply is_hset. apply funext. intro e. destruct (f e).
+Defined.
+
+Definition path_DType_simple A B (decA : DecidableEq A) (decB : DecidableEq B) (e : A = B) :
+  {| carrier := A; dec := decA |} = {| carrier := B; dec := decB |}.
+  apply path_DType. exists e. destruct e. apply dec_hprop. 
+Defined. 
+
+
+Definition dec_hprop_hprop A (decA : DecidableEq A) :
+  dec_hprop A decA decA = eq_refl.
+Admitted.
+
+Definition path_DType_simple_refl A (decA : DecidableEq A) :
+  path_DType_simple A A decA decA eq_refl = eq_refl.
+Proof.
+  unfold path_DType_simple. cbn. rewrite dec_hprop_hprop.
+  reflexivity. 
+Defined. 
+
+Definition path_DType_proj {A B : DType} :
+  A = B -> carrier A = carrier B.
+  destruct 1. reflexivity.
+Defined. 
+
+Definition path_DType_eq (A B:DType)
+           (e : @eq DType A B) :
+  match A,B return forall (e:A = B), Type with (Build_DType A decA),(Build_DType B decB)
+=> fun e => 
+     path_DType_simple A B decA decB (path_DType_proj e) = e end e.
+  destruct e, A. cbn. rewrite dec_hprop_hprop. reflexivity. 
+Defined. 
+
+Definition path_DType_eq' A B (decA : DecidableEq A) (decB : DecidableEq B)
+           (e : {| carrier := A; dec := decA |} = {| carrier := B; dec := decB |}) :
+  path_DType_simple _ _ _ _ (path_DType_proj e) = e := path_DType_eq _ _ e.
+
+Opaque path_DType_simple.
+
+Definition can_eq_eq_dec {A} {decA} (e :Canonical_eq A) : e.(can_eq) = Canonical_eq_decidable_ A.
+Proof.
+  apply funext; intros x. apply funext; intros y. apply funext; intro E.
+  destruct E.  rewrite can_eq_refl. unfold Canonical_eq_decidable_.
+  destruct (dec_paths x x). apply is_hset. destruct (f eq_refl).
+Defined.
+
+Definition Canonical_contr_dec A (ecanA : Canonical_eq A) (e : DecidableEq A) :
+  ecanA = Canonical_eq_decidable A.
+Proof.
+  unshelve eapply Canonical_eq_eq. cbn.
+  apply can_eq_eq_dec.
+  (* cbn. rewrite transport_paths_l. *)
+  (* unfold can_eq_eq_dec. cbn. apply inverse. *)
+  (* pose (@e_sect _ _ _ (funext _ _  (fun (x y : A) (e0 : eq A x y) => e0) (fun (x y : A) (e0 : eq A x y) => e0)) eq_refl). *)
+  (* etransitivity; try exact e0. clear e0. apply ap. apply funext. intros. cbn. *)
+  (* pose (@e_sect _ _ _ (funext _ _  (fun (y : A) (e0 : eq A x y) => e0) (fun (y : A) (e0 : eq A x y) => e0)) eq_refl). *)
+  (* etransitivity ; try apply e0. clear e0. apply ap. apply funext. intros y. cbn. *)
+  (* pose (@e_sect _ _ _ (funext _ _  (fun (e0 : eq A x y) => e0) (fun (e0 : eq A x y) => e0)) eq_refl). *)
+  (* etransitivity; try apply e0. clear e0. apply ap. apply funext. intros e0. cbn. *)
+  (* destruct e0. reflexivity. *)
+Admitted.
+
 Instance URDType_IsEq : URIsEq DType DType (Equiv_id _) _ URDType_Refl.
 Proof.
   intros A B. destruct A as [A decA], B as [B decB]. cbn.
-  apply admit. 
+  unshelve refine (isequiv_adjointify _ _ _ _).
+  - intros e. cbn in *. apply path_DType_simple. apply univalence. typeclasses eauto.
+  - intros e.  cbn.
+    refine (transport_eq (fun (E: {| carrier := A; dec := decA |} = {| carrier := B; dec := decB |}) => path_DType_simple A B decA decB
+    (e_inv (eq_to_equiv A B)
+           (equiv (transport_eq (fun X : DType => A ⋈ X) E (URType_Refl_decidable A decA)))) = E)
+                         (path_DType_eq _ _ e) _).
+    set (path_DType_proj e). cbn in *. apply ap. destruct e0. clear e. 
+    assert (decA = decB). apply dec_hprop. destruct X. cbn.
+    rewrite path_DType_simple_refl. 
+    exact (@e_sect _ _ _ (univalence _ _) eq_refl).
+  - intro e; cbn.
+    destruct e as [e eur ecoh ecanA ecanB].
+    revert eur ecoh ecanA ecanB. rewrite <- (@e_retr _ _ _ (univalence _ _) _).
+    set (eeq := (e_inv _ e)).
+    clearbody eeq;clear e.
+    destruct eeq. intros eur ecoh ecanA ecanB.
+    simpl.
+    destruct eur as [eur].
+    destruct ecoh as [ecoh].
+    simpl in *.
+    change (Equiv_id A) with (eq_to_equiv A A eq_refl).
+    rewrite (@e_sect _ _ _ (univalence _ _) _). simpl.
+    assert (decA = decB). apply dec_hprop. destruct X. cbn.
+    rewrite path_DType_simple_refl. cbn. 
+    unfold URType_Refl_decidable, URType_Refl_can, UR_gen. cbn. 
+    rewrite <- (@e_retr _ _ (e_fun (equiv_relation_equiv_fun _ _ _ _)) _ ecoh).
+    set (p := (e_inv _ ecoh)).
+    clearbody p. clear ecoh.
+    destruct p. cbn. 
+    assert (ecanA = Canonical_eq_decidable A) by apply Canonical_contr_dec.
+    assert (ecanB = Canonical_eq_decidable A) by apply Canonical_contr_dec.
+    destruct X, X0. 
+    reflexivity.
 Defined.
-
-(* ET: admit here *)
 
 Instance Canonical_eq_DType : Canonical_eq DType := Canonical_eq_gen _.
 
@@ -158,7 +253,7 @@ Defined.
 
 (* nat *)
 
-Instance DecidableEq_eq_nat : DecidableEq nat.
+Instance DecidableEq_eq_nat : DecidableEq (nat:Type).
 constructor. intros x y; revert y. 
 induction x.
 - destruct y.
