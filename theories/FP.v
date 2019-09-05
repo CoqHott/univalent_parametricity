eea7d96a0ba25c20cfefa4c4c85395c3f0b5209f
nicolas tabareau <nicolas.tabareau@inria.fr>
2019-09-05 11:14:21 +0200

    move lemmas on canonical eq

--- theories/FP.v
+++ theories/FP.v
@@ -9,47 +9,6 @@ Set Universe Polymorphism.
 Require Import HoTT CanonicalEq URTactics UnivalentParametricity.theories.UR UnivalentParametricity.theories.Transportable.
 
 
-(* Lemmas about canonical equality *)
-
-Definition can_eq_eq {A} (e :Canonical_eq A) : e.(can_eq) = fun x y e => e.
-Proof.
-  apply funext; intros x. apply funext; intros y. apply funext; intro E.
-  destruct E. apply can_eq_refl. 
-Defined. 
-
-
-Definition Canonical_eq_eq A (e e':Canonical_eq A)
-           (H : e.(can_eq) = e'.(can_eq)) :
-  (transport_eq (fun X => X = _) H  (can_eq_eq e) = (can_eq_eq e')) ->
-  e = e'.
-Proof.
-  destruct e, e'. cbn in *. destruct H. cbn.
-  unfold can_eq_eq.
-  intros H. apply ap_inv_equiv' in H. cbn in H. 
-  assert (can_eq_refl = can_eq_refl0).
-  apply funext. intro x. 
-  pose (H' := apD10 H x). apply ap_inv_equiv' in H'.
-  pose (H'' := apD10 H' x). apply ap_inv_equiv' in H''.
-  exact (apD10 H'' eq_refl). 
-  destruct X. reflexivity.
-Defined. 
-
-Definition Canonical_contr A (e :Canonical_eq A) : e = Canonical_eq_gen A.
-Proof.
-  unshelve eapply Canonical_eq_eq.
-  apply can_eq_eq.
-  cbn. rewrite transport_paths_l. rewrite inv_inv.
-  unfold can_eq_eq. cbn. apply inverse. 
-  pose (@e_sect _ _ _ (funext _ _  (fun (x y : A) (e0 : eq A x y) => e0) (fun (x y : A) (e0 : eq A x y) => e0)) eq_refl).
-  etransitivity; try exact e0. clear e0. apply ap. apply funext. intros. cbn.
-  pose (@e_sect _ _ _ (funext _ _  (fun (y : A) (e0 : eq A x y) => e0) (fun (y : A) (e0 : eq A x y) => e0)) eq_refl).
-  etransitivity ; try apply e0. clear e0. apply ap. apply funext. intros y. cbn.
-  pose (@e_sect _ _ _ (funext _ _  (fun (e0 : eq A x y) => e0) (fun (e0 : eq A x y) => e0)) eq_refl). 
-  etransitivity; try apply e0. clear e0. apply ap. apply funext. intros e0. cbn.
-  destruct e0. reflexivity.                  
-Defined.
-
-
 (*! Establishing FP for Type !*)
 
 Definition transport_UR_Type A B C (e: B = C) e1 e2 e3 e4 e5 :
